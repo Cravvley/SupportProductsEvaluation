13b58198f793437e680cd263d3fd78ada1661b0c
Daniel Niekurzak <niekurzakdaniel@gmail.com>
2020-10-06 17:34:35 +0200

    added CategoryService

---
+++ SupportProductsEvaluation.Infrastructure/Services/CategoryService.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using SupportProductsEvaluation.Core.Entities;
+using SupportProductsEvaluation.Core.Repositories;
+using SupportProductsEvaluation.Infrastructure.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SupportProductsEvaluation.Infrastructure.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+        public async Task Create(Category category)
+        {
+            if (category == null)
+            {
+                throw new ArgumentNullException("category doesn't exist");
+            }
+            await _categoryRepository.Create(category);
+        }
+
+        public async Task Delete(int? id)
+        {
+            var categoryEntity = await _categoryRepository.Get(id);
+            if (categoryEntity == null)
+            {
+                throw new ArgumentNullException("category doesn't exist");
+            }
+            await _categoryRepository.Delete(id);
+        }
+
+        public async Task<Category> Get(int? id)
+        {
+            var categoryEntity = await _categoryRepository.Get(id);
+            if (categoryEntity == null)
+            {
+                throw new ArgumentNullException("category doesn't exist");
+            }
+            return categoryEntity;
+        }
+
+        public async Task<IList<Category>> GetAll()
+            => await _categoryRepository.GetAll();
+        public async Task<bool> IsExist(Category category)
+        {
+            var categoryEntity = await _categoryRepository.Get(category);
+            if (categoryEntity == null)
+            {
+                return false;
+            }
+            return true;
+        }
+        public async Task Update(Category category)
+        {
+            var categoryEntity = await _categoryRepository.Get(category.Id);
+            if (categoryEntity == null)
+            {
+                throw new ArgumentNullException("category doesn't exist");
+            }
+            await _categoryRepository.Update(category);
+        }
+    }
+}
---
+++ SupportProductsEvaluation.Infrastructure/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using SupportProductsEvaluation.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SupportProductsEvaluation.Infrastructure.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task Create(Category category);
+        Task Delete(int? id);
+        Task Update(Category category);
+        Task<Category> Get(int? id);
+        Task<IList<Category>> GetAll();
+        Task<bool> IsExist(Category category);
+    }
+}
