81450f1f1e9230a5308d7042e0fcfc82e40d2139
Daniel Niekurzak <niekurzakdaniel@gmail.com>
2020-10-08 16:19:47 +0200

    added report model

---
+++ SupportProductsEvaluation.Core/Entities/Report.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace SupportProductsEvaluation.Core.Entities
+{
+    public class Report
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Product name")]
+        public string ProductName { get; set; }
+        [Required]
+        [Display(Name = "Category name")]
+        public string CategoryName { get; set; }
+        [Required]
+        [Display(Name = "Subcategory name")]
+        public string SubCategoryName { get; set; }
+        [Required]
+        public double AvgPrice { get; set; }
+        [Required]
+        public double AvgRate { get; set; }
+        [Required]
+        [Display(Name = "Update at")]
+        public DateTime UpdateAt { get; set; }
+
+    }
+}
